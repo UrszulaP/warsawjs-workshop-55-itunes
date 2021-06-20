1030957921f0a2420641dcfef90cbffb9093edaa
UrszulaP <urszulapoliszuk@gmail.com>
2021-06-20 14:36:23 +0200

    add modal, use box instead of table

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import {
   Badge,
   Stack,
   Button,
-  UnorderedList,
-  ListItem,
   Input,
-  
+
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
-  TableCaption,
-  
+
   Box,
   Spinner,
+  Center,
+  SimpleGrid,
+
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  AspectRatio,
+  useDisclosure,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function LoadingSpinner(props) {
@@ -31,50 +39,121 @@ function LoadingSpinner(props) {
 }
 
 
-function Itunes() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [results, setResults] = useState([
-    {
-      trackName: "WarsawJS",
-    },
-    {
-      trackName: "React",
-    },
-    {
-      trackName: "Itunes",
-    },
-  ]);
-  const [isLoading, setIsLoading] = useState(false);
+
+function DetailsModal({ result }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [artist, setArtist] = useState();
+
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+
+    (async () => {
+      const artistResult = await fetch(
+        `https://itunes.apple.com/lookup?id=${result.artistId}`
+      );
+      const data = await artistResult.json();
+      setArtist(data.results[0]);
+    })();
+  }, [isOpen, result]);
 
   return (
-    <Stack direction="column">
-      <Stack direction="row">
-        <Input
-          value={searchTerm}
-          onChange={(event) => {
-            setSearchTerm(event.target.value);
-          }}
-        />
-        <Button
-          colorScheme="blue" colorScheme="teal"
-          onClick={async () => {
-            setIsLoading(true);
-            const result = await fetch(
-              `https://itunes.apple.com/search?term=${encodeURIComponent(
-                searchTerm
-              )}&entity=musicVideo`
-            );
-            const data = await result.json();
-            setIsLoading(false);
-            setResults(data.results);
-            console.log(data);
-          }}
-        >
-          Search <LoadingSpinner isLoadingg={isLoading} />
-        </Button>    
-      </Stack>
-      
-      <Box maxW="lg" borderWidth="1px" borderRadius="lg" pt="3">
+    <>
+      <Button colorScheme="teal" onClick={onOpen} size="sm">
+        Show video
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{result.trackName}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {artist && (
+              <Badge>
+                {artist.artistName} ({artist.primaryGenreName})
+              </Badge>
+            )}
+            <AspectRatio maxW="560px" ratio={1}>
+              <iframe title="naruto" src={result.previewUrl} allowFullScreen />
+            </AspectRatio>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+
+function Itunes() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const property = {
+    imageUrl: "https://bit.ly/2Z4KKcF",
+    imageAlt: "Rear view of modern home with pool",
+    beds: 3,
+    baths: 2,
+    title: "Modern home in city center in the heart of historic Los Angeles",
+    formattedPrice: "$1,900.00",
+    reviewCount: 34,
+    rating: 4,
+  }
+
+  return (
+    <Stack direction="column" mt="5">
+      <Center>
+        <Stack direction="row" maxW="sm" mb="5">
+          <Input
+            value={searchTerm}
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+          <Button
+            colorScheme="teal"
+            onClick={async () => {
+              setIsLoading(true);
+              const result = await fetch(
+                `https://itunes.apple.com/search?term=${encodeURIComponent(
+                  searchTerm
+                )}&entity=musicVideo`
+              );
+              const data = await result.json();
+              setIsLoading(false);
+              setResults(data.results);
+              console.log(data);
+            }}
+          >
+            Search <LoadingSpinner isLoadingg={isLoading} />
+          </Button>
+        </Stack>
+      </Center>
+
+      <SimpleGrid columns={3} spacing={5}>
+      {results.map((result) => (
+        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <Box p="6">
+            <Box mb="3">
+             <Center><img src={result.artworkUrl100} /></Center>
+            </Box>
+            <Box fontSize="sm" color="gray.500">
+              {result.artistName}
+            </Box>
+            <Box fontSize="lg">
+              {result.trackName}
+            </Box>
+            <Box mt="3">
+              <DetailsModal result={result} />
+            </Box>
+          </Box>
+        </Box>
+      ))}
+      </SimpleGrid>
+
+
+      {/* <Box maxW="lg" borderWidth="1px" borderRadius="lg" pt="3">
         <Table variant="simple" size="sm">
           <Thead>
             <Tr>
@@ -87,45 +166,24 @@ function Itunes() {
               <Tr>
                 <Td>{result.trackName}</Td>
                 <Td>{result.artistName}</Td>
+                <Td><DetailsModal result={result} /></Td>
               </Tr>
             ))}
           </Tbody>
         </Table>
-      </Box>
+      </Box> */}
+
     </Stack>
   );
 }
 
 function App(props) {
-  // const result = useState(0);
-  // const count = result[0];
-  // const setCount = result[1];
-  // const [count, setCount] = useState(5);
 
   return (
     <div className="App">
       <header className="App-header">
         <Stack direction="column">
           <Itunes />
-          {/* <Stack direction="row">
-            <Badge>{count}</Badge>
-            <Button
-              colorScheme="green"
-              onClick={() => {
-                setCount(count + 1);
-              }}
-            >
-              +
-            </Button>
-            <Button
-              colorScheme="red"
-              onClick={() => {
-                setCount(count - 1);
-              }}
-            >
-              -
-            </Button>
-          </Stack> */}
         </Stack>
       </header>
     </div>
