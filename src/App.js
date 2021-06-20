38a7b5ce165e906645793e911d844fed26df8222
UrszulaP <urszulapoliszuk@gmail.com>
2021-06-20 12:33:29 +0200

    create project base

--- src/App.js
+++ src/App.js
@@ -1,22 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Badge,
+  Stack,
+  Button,
+  UnorderedList,
+  ListItem,
+  Input,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-function App() {
+function Itunes() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([
+    {
+      trackName: "WarsawJS",
+    },
+    {
+      trackName: "React",
+    },
+    {
+      trackName: "Itunes",
+    },
+  ]);
+
+  return (
+    <Stack direction="column">
+      <Stack direction="row">
+        <Input
+          value={searchTerm}
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        />
+        <Button
+          colorScheme="blue"
+          onClick={async () => {
+            const result = await fetch(
+              `https://itunes.apple.com/search?term=${encodeURIComponent(
+                searchTerm
+              )}&entity=musicVideo`
+            );
+            const data = await result.json();
+            setResults(data.results);
+          }}
+        >
+          Search
+        </Button>
+      </Stack>
+      <UnorderedList>
+        {results.map((result) => (
+          <ListItem>
+            {result.trackName} ({result.artistName})
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </Stack>
+  );
+}
+
+function App(props) {
+  // const result = useState(0);
+  // const count = result[0];
+  // const setCount = result[1];
+  const [count, setCount] = useState(5);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Stack direction="column">
+          <Itunes />
+          <Stack direction="row">
+            <Badge>{count}</Badge>
+            <Button
+              colorScheme="green"
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              +
+            </Button>
+            <Button
+              colorScheme="red"
+              onClick={() => {
+                setCount(count - 1);
+              }}
+            >
+              -
+            </Button>
+          </Stack>
+        </Stack>
       </header>
     </div>
   );
